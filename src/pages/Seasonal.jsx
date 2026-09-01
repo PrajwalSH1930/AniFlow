57ff6f65c88b9958d85ae4312c8ee50b4d19435b
Prajwal Hiremath <psh23g@gmail.com>
2026-09-01 12:20:44 +0530

    Bugs fixed

--- src/pages/Seasonal.jsx
+++ src/pages/Seasonal.jsx
@@ -190,7 +190,7 @@ export default function Seasonal() {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 -mx-10 sm:gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 -mx-10 sm:mx-0 sm:gap-6">
           {loading
             ? Array.from({ length: ITEMS_PER_PAGE }).map((_, idx) => (
                 <AnimeCardSkeleton key={idx} />
